d08c0e16192f044ac1f370bc9544fba5a1c8542c
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-12-28 01:33:43 +0530

    push

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { generateSW } from './pwa.mjs'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-node'
 
 const config = {
 	preprocess: vitePreprocess(),
