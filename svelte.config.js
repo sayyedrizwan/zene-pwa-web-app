8b7cb233cff826ab461d4e6e2d05d5c2e932c7a4
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-12-28 01:38:55 +0530

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { generateSW } from './pwa.mjs'
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-vercel'
 
 const config = {
 	preprocess: vitePreprocess(),
