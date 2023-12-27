41faa83327922a8af75aec7614dc26adf48062f6
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-12-28 01:21:10 +0530

    rm adapter

--- adapter.mjs
+++
@@ -1,7 +0,0 @@
-import process from 'node:process'
-import AdapterNode from '@sveltejs/adapter-node'
-import AdpaterStatic from '@sveltejs/adapter-static'
-
-export const nodeAdapter = process.env.NODE_ADAPTER === 'true'
-
-export const adapter = nodeAdapter ? AdapterNode() : AdpaterStatic({strict: true})
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { generateSW } from './pwa.mjs'
-import { adapter } from './adapter.mjs'
 
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter,
 		serviceWorker: {
 			register: false,
 		},
