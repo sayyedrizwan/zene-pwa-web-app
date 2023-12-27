fa4e2f2e63cb003f48852056ff03a0b96c5f2607
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-12-28 00:53:41 +0530

    completed PWA in the web app

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,17 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+import 'vite-plugin-pwa/svelte'
+import 'vite-plugin-pwa/info'
+
 declare global {
+	declare const __DATE__: string
+	declare const __RELOAD_SW__: boolean
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {
+			userid: string;
+			buildDate: string;
+			periodicUpdates: boolean;
+		}
 	}
 }
+
 
 export {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,74 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { generateSW } from './pwa.mjs'
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA({
-		strategies: 'injectManifest',
-		srcDir: 'src',
-		filename: 'service-worker.js'
-	})],
+	logLevel: 'info',
+	build: {
+		minify: false,
+	},
 	define: {
-		"process.env.NODE_ENV": '"production"',
-	}
+		__DATE__: `'${new Date().toISOString()}'`,
+		__RELOAD_SW__: false,
+		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"',
+	},
+	server: {
+		fs: {
+			allow: ['../..']
+		}
+	},
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			srcDir: './src',
+			mode: 'development',
+			strategies: generateSW ? 'generateSW' : 'injectManifest',
+			filename: generateSW ? undefined : 'prompt-sw.ts',
+			scope: '/',
+			base: '/',
+			selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+			manifest: {
+				short_name: 'SvelteKit PWA',
+				name: 'SvelteKit PWA',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				icons: [
+					{
+						src: '/logo192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/logo512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: '/logo512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+			injectManifest: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			devOptions: {
+				enabled: true,
+				suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
+				type: 'module',
+				navigateFallback: '/',
+			},
+			kit: {
+				includeVersionFile: true,
+			}
+		})],
 });
