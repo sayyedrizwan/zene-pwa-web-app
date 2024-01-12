480a4475a9a17cdcb5c01ad443bfab40bdd1541b
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-01-12 13:05:56 +0530

    added hover animation

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,7 +9,7 @@
         <h2 class="font-bold text-3xl xl:text-4xl leading-snug">
             {are_you_ready}?<br>{to_dive_into_some_fresh_tunes}
         </h2>
-        <a class="mt-8 xl:mt-12 px-12 py-5 text-lg font-medium leading-tight inline-block bg-blue-800 rounded-full shadow-xl border border-transparent hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-sky-999 focus:ring-sky-500 transition-all ease-in-out delay-75 duration-300 hover:bg-gray-950" href="/">{open_zene__web_player}</a>
+        <a class="mt-8 xl:mt-12 px-12 py-5 text-lg font-medium leading-tight inline-block bg-blue-800 rounded-full shadow-xl border border-transparent hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-sky-999 focus:ring-sky-500 hover-animation hover:bg-gray-950" href="/">{open_zene__web_player}</a>
         <div class="mt-14 xl:mt-20">
             <nav class="flex flex-wrap justify-center text-lg font-medium">
                 <div class="px-5 py-2"><a href="/privacy-policy">{policy}</a></div>
@@ -17,7 +17,7 @@
                 <div class="px-5 py-2"><a href="/faq">{faq}</a></div>
             </nav>
             <nav class="flex flex-wrap justify-center mt-5">
-                <a href={instagram_account} target="_blank"><i class="fa fa-instagram text-white hover:text-peach transition-all ease-in-out delay-75 duration-300" style="font-size: 24px;"></i></a>
+                <a href={instagram_account} target="_blank"><i class="fa fa-instagram text-white hover:text-peach hover-animation" style="font-size: 24px;"></i></a>
             </nav>
             <p class="mt-7 text-base">© 2022-2023 Zene from rizwan sayyed</p>
         </div>
