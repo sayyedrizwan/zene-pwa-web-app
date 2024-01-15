d4bf3f9ee473282511131c6eb7c9dd273af46596
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-01-15 19:03:03 +0530

    fixed UI for iPhone

--- src/lib/components/main-page/TopListeningSongs.svelte
+++ src/lib/components/main-page/TopListeningSongs.svelte
@@ -5,8 +5,10 @@
   import MenuIcon from '$lib/assets/img/ic_menu.svg'
   import { type ResponseData, ResponseDataEnum } from '../../../domain/RequestEnumClass'
   import type { TopSongsMusicResults } from '../../../domain/local/entities/TopSongsMusic'
+  import SongInfoSheet from '../global-view/SongInfoSheet.svelte'
 
   export let authKey: string
+  let showSheet: boolean = false
 
   let response: ResponseData<TopSongsMusicResults> = { type: ResponseDataEnum.EMPTY }
 
@@ -31,12 +33,12 @@
 </script>
 
 <div class="md:bg-maincolor rounded-ss-lg w-full md:w-1/3 md:h-full md:fixed md:overflow-y-auto">
-  <h3 class="text-white urbanist-semibold text-3xl ms-4 mt-7">Discover the Currently <br />Most Playing Songs on Zene</h3>
+  <h3 class="text-white urbanist-semibold text-xl md:text-3xl ms-2 md:ms-4 mt-7">Discover the Currently <br />Most Playing Songs on Zene</h3>
 
   <div class="overflow-x-auto flex scrollbar-hide">
     {#if response.type == ResponseDataEnum.LOADING}
       {#each Array(5) as _, index (index)}
-        <div class="flex-none py-6 px-3 first:pl-6 last:pr-6">
+        <div class="flex-none py-6 px-1 first:pl-6 last:pr-6">
           <div class="flex flex-col items-center justify-center gap-2">
             <div class="w-[16rem] h-[22rem] bg-gray-400 animate-pulse rounded-lg" />
           </div>
@@ -46,25 +48,24 @@
       <h1>{response.type}</h1>
     {:else if response.type == ResponseDataEnum.SUCCESS}
       {#each response.data.music as item}
-        <div class="flex-none py-6 px-3 first:pl-6 last:pr-6">
+        <div class="flex-none py-6 px-1 first:pl-2 last:pr-3">
           <div class="flex flex-col items-center justify-center gap-2 cursor-pointer">
             <div class={`relative w-[16rem] h-[22rem] bg-black rounded-lg`}>
-              <img src={item.artistsImg} alt={item.music?.artists} class="absolute top-0 left-0 w-full h-full object-cover rounded-lg"/>
-              <div class="absolute top-0 left-0 w-full h-full rounded-lg bg-opacity-60 md:bg-opacity-30 bg-maincolor hover-animation"/>
+              <img src={item.artistsImg} alt={item.music?.artists} class="absolute top-0 left-0 w-full h-full object-cover rounded-lg" />
+              <div class="absolute top-0 left-0 w-full h-full rounded-lg bg-opacity-60 md:bg-opacity-30 bg-maincolor hover-animation" />
               <div class="absolute top-0 left-0 w-full h-full rounded-lg md:bg-opacity-10 md:bg-maincolor md:hover:bg-opacity-50 hover-animation">
                 <p class="text-white urbanist-thin text-sm mt-2 ms-2">{item.music?.artists}</p>
                 <p class="text-white urbanist-semibold text-xl ms-1.5">{item.music?.name}</p>
                 <div class="flex items-center mt-2 ms-1">
-                  <img src={PlayIcon} class="size-5" alt="play"/>
+                  <img src={PlayIcon} class="size-5" alt="play" />
                   <p class="text-white urbanist-regular text-sm">{item.listeners} Listeners</p>
                 </div>
-              
-                <div class="absolute bottom-0 left-0 flex p-3 w-full">
-                <button><img src={MenuIcon} class="size-9" alt="menu"/></button>
-                <div class="w-full" />
-                <img src={item.music?.thumbnail} alt={item.music?.artists} referrerpolicy="no-referrer" class="rounded-full size-10 object-cover"/>
-              </div>
 
+                <div class="absolute bottom-0 left-0 flex p-3 w-full">
+                  <button id="open-sheet" aria-controls="sheet"><img src={MenuIcon} class="size-9" alt="menu" /></button>
+                  <div class="w-full" />
+                  <img src={item.music?.thumbnail} alt={item.music?.artists} referrerpolicy="no-referrer" class="rounded-full size-10 object-cover" />
+                </div>
               </div>
             </div>
           </div>
@@ -75,3 +76,9 @@
     {/if}
   </div>
 </div>
+
+
+
+<!-- {#if showSheet} -->
+  <SongInfoSheet />
+<!-- {/if} -->
