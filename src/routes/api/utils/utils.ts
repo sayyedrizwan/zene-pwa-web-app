7938d2393af06130f02e9a969dca04520e2e607b
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-01-18 23:41:29 +0530

    added new package-json and node-moule

--- src/routes/api/utils/utils.ts
+++ src/routes/api/utils/utils.ts
@@ -21,7 +21,7 @@ export const lastfm_top_playing_songs = "https://kerve.last.fm/kerve/charts?type
 export const ip_base_url = "http://ip-api.com/json/"
 
 
-export const top_100_artists_billboard = "hhttps://www.billboard.com/charts/artist-100/"
+export const top_100_artists_billboard = "https://www.billboard.com/charts/artist-100/"
 
 
 export const radio_browser_url = "_api._tcp.radio-browser.info"
