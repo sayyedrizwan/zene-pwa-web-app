94487498fc579659077d5ab31c8f2f924769706a
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-01-22 10:54:18 +0530

    completed the search options

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import { browser } from '$app/environment'
   import { env } from '$env/dynamic/public'
+  import MIC from '$lib/assets/img/ic_mic.svg'
+  import MUSIC_NOTE from '$lib/assets/img/ic_music_note.svg'
+  import SEARCH from '$lib/assets/img/ic_search.svg'
+  import ARROW_UP_RIGHT from '$lib/assets/img/ic-arrow-up-right.svg'
   import axios from 'axios'
   import { ResponseDataEnum, type ResponseData } from '../../domain/RequestEnumClass'
 
@@ -8,25 +12,34 @@
   let key: string | null = null
 
   let searchSuggestion: ResponseData<string[]> = { type: ResponseDataEnum.EMPTY }
+  let timeout: NodeJS.Timeout | null
 
   if (browser) key = window.atob(data.data)
 
-  function onSubmit(){
-    const searchValue = document.getElementById("voice-search") as HTMLInputElement
+  function onSubmit() {
+    const searchValue = document.getElementById('voice-search') as HTMLInputElement
     console.log(searchValue.value)
   }
 
-  async function onSearch(){
-    const searchValue = document.getElementById("voice-search") as HTMLInputElement
-    if(searchValue.value.trim().length <= 3) return
-
+  async function searchSuggestions() {
+    searchSuggestion = { type: ResponseDataEnum.LOADING }
+    const searchValue = document.getElementById('voice-search') as HTMLInputElement
     try {
-      const res = await axios({method: 'post', url: env.PUBLIC_SEARCH_KEYWORDS, headers: { AuthorizationKey: key } , data: { q: searchValue.value.trim() } })
-    const response = await res.data as string[]
-    console.log(response) 
+      const res = await axios({ method: 'post', url: env.PUBLIC_SEARCH_KEYWORDS, headers: { AuthorizationKey: key }, data: { q: searchValue.value.trim() } })
+      const response = (await res.data) as string[]
+      searchSuggestion = { type: ResponseDataEnum.SUCCESS, data: response }
     } catch (error) {
-      console.log(error)
+      searchSuggestion = { type: ResponseDataEnum.ERROR }
     }
+  }
+
+  async function onSearch() {
+    const searchValue = document.getElementById('voice-search') as HTMLInputElement
+    if (searchValue.value.trim().length <= 3) return
+
+    console.log('startedd')
+    if (timeout != null) clearTimeout(timeout)
+    timeout = setTimeout(searchSuggestions, 1000)
   }
 </script>
 
@@ -36,38 +49,45 @@
       <label for="voice-search" class="sr-only">Search</label>
       <div class="relative w-full">
         <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z"
-            />
-          </svg>
+          <img src={MUSIC_NOTE} alt="mic" class="size-5" />
         </div>
         <input
-          on:change={onSearch}
+          on:input={onSearch}
           autocomplete="off"
           type="text"
           id="voice-search"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search Mockups, Logos, Design Templates..."
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
+          placeholder="Search songs, artists, radio...."
           required
         />
         <button type="button" class="absolute inset-y-0 end-0 flex items-center pe-3">
-          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z" />
-          </svg>
+          <img src={MIC} alt="mic" class="size-5" />
         </button>
       </div>
-      <button type="submit" class="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-maincolor rounded-lg border border-blue-700">
-        <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-        </svg>Search
+      <button type="submit" class="inline-flex items-center py-2.5 px-3 pe-6 ms-2 text-sm font-medium text-white bg-maincolor rounded-lg border border-blue-700">
+        <img src={SEARCH} alt="mic" class="size-4 me-2" />Search
       </button>
     </form>
   </center>
+
+  {#if searchSuggestion.type == ResponseDataEnum.LOADING}
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+      {#each Array(6) as _, index (index)}
+        <div class="bg-gray-600 pl animate-pulse rounded-lg w-full h-10" />
+      {/each}
+    </div>
+  {:else if searchSuggestion.type == ResponseDataEnum.SUCCESS}
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+      {#each searchSuggestion.data as item}
+        <button class="p-3 text-gray-300 flex cursor-pointer md:border-2 border-maincolor rounded-xl md:hover:bg-maincolor hover-animation">
+          <img src={SEARCH} alt="mic" class="size-5 me-2 translate-y-1" />
+          <p class="urbanist-semibold text-base md:text-lg ms-1 w-full text-start">{item}</p>
+
+          <img src={ARROW_UP_RIGHT} alt="mic" class="size-5 me-2 translate-y-1" />
+        </button>
+      {/each}
+    </div>
+  {/if}
 
   <div class="h-60" />
 {/if}
