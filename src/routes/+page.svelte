a3910ebc8df7dfd15efbdd005ec048cc67cbce58
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-01-26 23:36:46 +0530

    added header on zene

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
   <meta name="description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
   <meta name="og:description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
   <meta property="og:title" content="Zene: A Free Muic App" />
-  <meta property="og:image" content="%sveltekit.assets%/logo820.png" />
+  <meta property="og:image" content="https://zenemusic.co/logo820.png" />
   <meta property="og:image:alt" content="Zene Logo" />
   <link rel="canonical" href="https://zenemusic.co" />
 </svelte:head>
