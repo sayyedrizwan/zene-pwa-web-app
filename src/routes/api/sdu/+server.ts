30c58bd138f5cd8c08f3f32cb1ead9a8f57a2c68
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-01-27 23:13:07 +0530

    completed song playing.

--- src/routes/api/sdu/+server.ts
+++ src/routes/api/sdu/+server.ts
@@ -3,7 +3,7 @@ import { atob } from 'buffer'
 import ytdl from 'ytdl-core'
 
 
-export const GET = (async (req: RequestEvent, res: Response) => {
+export const GET = (async (req: RequestEvent) => {
   const video_url = new URL(req.url).searchParams.get('id') ?? ""
 
   let audioStream = ytdl(`https://www.youtube.com/watch?v=${atob(video_url)}`, { quality: "highestaudio" });
