848b07006824ce907d520cc1ec3ebfb142fb222d
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-01-27 17:30:44 +0530

    check the ip adress to run test #4222

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,11 +31,9 @@
       const song = (event as CustomEvent).detail.value as MusicData
     
       try {
-        const headers = { timeout: 30000, headers: { id: song.songId, i: data.data } }
-        const res = await axios.get(env.PUBLIC_DOWNLOAD_URL)
-        // const res = await axios.post(env.PUBLIC_DOWNLOAD_URL, {}, headers)
-        // const url = window.atob(res.data)
-        console.log(res.data)
+        const res = await axios.get(`${env.PUBLIC_DOWNLOAD_URL}?id=${window.btoa(song.songId ?? "")}`)
+        const url = window.atob(res.data)
+        console.log(url)
         // audioPlayer?.play(res.data as string, song)
 
         // audioPlayer?.play(window.atob(res.data), song)
--- src/routes/api/sdu/+server.ts
+++ src/routes/api/sdu/+server.ts
@@ -1,5 +1,5 @@
 import { json, type RequestEvent } from '@sveltejs/kit'
-import { btoa } from 'buffer'
+import { atob, btoa } from 'buffer'
 import fs  from 'fs'
 import type { RequestHandler } from './$types'
 import ytdl from 'ytdl-core'
@@ -25,12 +25,21 @@ export async function POST(events: RequestEvent) {
   }
 }
 
-export async function GET(events: RequestEvent) {
-     
+
+export const GET = (async ( url: RequestEvent ) => {
+	const video_url = new URL(url.url).searchParams.get('id') ?? ""
+
   try {
-    const value = await ytdl.getInfo("https://www.youtube.com/watch?v=g4oH5xKwm9A&ab_channel=SharkTankIndia")
-    return json(value);
-  }catch(error){
-    return json({})
+    const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${atob(video_url)}`)
+    let url: string = ''
+
+    info.formats.forEach((element) => {
+      if (element.hasAudio === true && element.qualityLabel === '360p' && url === '') {
+        url = element.url
+      }
+    })
+    return new Response(btoa(url))
+  } catch (error) {
+    return new Response('')
   }
-}
+}) satisfies RequestHandler
