855bdd45005937126543f3fdeaecc9664cfcb450
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-03-01 22:24:23 +0530

    push temp #4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,8 +44,8 @@
 {#if authKey != null}
 
   <audio controls preload="auto">
-    <source src="http://localhost:3419/api/downloads" type="audio/ogg" />
-    <source src="http://localhost:3419/api/downloads" type="audio/mpeg" />
+    <source src="/api/downloads" type="audio/ogg" />
+    <source src="/api/downloads" type="audio/mpeg" />
     Your browser does not support the audio element.
   </audio>
 
