23a015ee1dd17e937eca79e7c6b642e503c1b289
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-03-19 00:33:45 +0530

    fixed songplaying..

--- src/routes/api/api_impl/yt_downloader/YtDownloaderImpl.ts
+++ src/routes/api/api_impl/yt_downloader/YtDownloaderImpl.ts
@@ -6,8 +6,8 @@ import axios, { type AxiosResponse } from 'axios'
 import type { CA3ConvertorResponse } from './domain/CA3ConvertorResponse'
 
 export class YTDownloaderImpl {
-  async videoURL(videoId: string, isSameServer: Boolean, isSafari: Boolean) {
-    const path = await this.videoYTDownloader(videoId, isSameServer, isSafari)
+  async videoURL(videoId: string, isSameServer: Boolean) {
+    const path = await this.videoYTDownloader(videoId, isSameServer)
     if (path != null) return path
 
     try {
@@ -49,76 +49,16 @@ export class YTDownloaderImpl {
     }
   }
 
-  async videoYTDownloader(videoId: string, isSameServer: Boolean, isSafari: Boolean): Promise<string | null> {
+  async videoYTDownloader(videoId: string, isSameServer: Boolean): Promise<string | null> {
     try {
       let info = await ytdl.getInfo(videoId)
       let audioFormats = ytdl.filterFormats(info.formats, 'audioonly')
-      console.log(audioFormats)
       let url = audioFormats.findLast((a) => a.mimeType?.includes('audio/mp4; codecs='))?.url
       
       return url ?? null
-
-      // if (isSameServer == true && (url ?? null != null)) return url ?? ''
-
-      // const fileSize = await getFileSize(url!)
-      // const chucks = await downloadBlobInChunks(url!, 1000000, fileSize!)
-      // const blob = new Blob(chucks, { type: 'audio/mp4' })
-
-      // return blob
-      // if (isSafari) {
-      //   const convert = await convertedMP3(blob)
-      //   if (convert != null) return convert.trim()
-      // }
-
-      // const responseUid = await fetch('https://wsend.net/createunreg', {
-      //   method: 'POST',
-      //   headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
-      //   body: 'start=1',
-      // })
-      // const uid = await responseUid.text()
-
-      // const formData = new FormData()
-      // formData.append('filehandle', blob, 'videoplayback.mp4')
-      // formData.append('uid', uid)
-      // const request = await fetch('https://wsend.net/upload_cli', { method: 'POST', headers: {}, body: formData })
-      // const response = await request.text()
-      // return response.trim()
     } catch (error) {
       return null
     }
-  }
-}
-
-async function convertedMP3(blob: Blob): Promise<string | null> {
-  try {
-    const formdata = new FormData()
-    formdata.append('files[]', blob, `${generateRandomString(20)}_${generateRandomString(20)}.mp4`)
-    formdata.append('target', 'djvu')
-    formdata.append('ajax', '1')
-    formdata.append('ajax', '1')
-
-    const convertor = await fetch('https://ca3.converter.app/mp4-to-mp3/uploader.php', { method: 'POST', body: formdata })
-    const response = (await convertor.json()) as CA3ConvertorResponse
-
-    await fetch(`https://ca3.converter.app/mp4-to-mp3/process.php?jobid=${response.jobid}`, { method: 'POST' })
-    let isConvertorRunning = true
-
-    setTimeout(() => {
-      isConvertorRunning = false
-    }, 2000)
-
-    while (isConvertorRunning) {
-      await waitServer(600)
-      const responseConverter = await fetch(`https://ca3.converter.app/mp4-to-mp3/display.php?jobid=${response.jobid}`, { method: 'POST' })
-      if ((await responseConverter.text()).trim() == '100') isConvertorRunning = false
-    }
-
-    await waitServer(1000)
-
-    return `https://ca3.converter.app/download.php?jobid=${response.jobid}`
-  } catch (error) {
-    console.log(error)
-    return null
   }
 }
 
@@ -126,6 +66,7 @@ export async function getFileSize(url: string): Promise<number | null> {
   try {
     const response = await axios.head(url)
     const contentLength = response.headers['content-length']
+    console.log(response.headers)
     return contentLength ? parseInt(contentLength, 10) : null
   } catch (error) {
     return null
--- src/routes/api/sdu/+server.ts
+++ src/routes/api/sdu/+server.ts
@@ -5,86 +5,32 @@ import { downloadBlobInChunks, getFileSize, YTDownloaderImpl } from '../api_impl
 import { isFromZeneOrigin } from '../utils/EncryptionForAPI'
 import { DURLResponse } from '../../../domain/local/entities/DURLResponse'
 import { isSameServerIp } from '../utils/utils'
-import got from 'got'
+import axios from 'axios'
 
 export const GET = async (req: RequestEvent) => {
+  const video_url = new URL(req.url).searchParams.get('id') ?? ''
+  const videoId = video_url.length > 20 ? video_url : atob(video_url)
+
+  if (videoId.length > 20 && videoId.split('-').length > 3) {
+    const radio = new RadioBrowserImpl()
+    const response = await radio.radioPlayURL(videoId)
+    const url = response[0].url_resolved == null ? response[0].url : response[0].url_resolved
+    // const blob = new Blob(url, { type: 'audio/mpeg' })
+    // return new Response(blob, { status: 200, headers: header })
+  }
+
   const ytDownloader = new YTDownloaderImpl()
-  const url = await ytDownloader.videoYTDownloader('YALvuUpY_b0', false, false)
+  const url = await ytDownloader.videoURL(videoId, false)
+  const responseInfo = await axios.head(url!)
 
-  const fileSize = await getFileSize(url!)
-  const response = await downloadBlobInChunks(url!, 1000000, fileSize!)
+  const response = await downloadBlobInChunks(url!, 2000000, responseInfo.headers['content-length'])
+  const blob = new Blob(response, { type: 'audio/mpeg' })
 
-  // const response = await fetch('https://rr1---sn-w2gqpbpu5a-cvhl.googlevideo.com/videoplayback?expire=1710806107&ei=-3_4ZaGKJba54t4P2PuNmAo&ip=183.87.181.110&id=o-AFX6TSDfp9X3Y0ZtZpGsBFxUuWgSzB3bkBm_9pMIP8FS&itag=140&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=io&mm=31%2C29&mn=sn-w2gqpbpu5a-cvhl%2Csn-cvh7knzd&ms=au%2Crdu&mv=m&mvi=1&pl=24&gcr=in&initcwndbps=1073750&spc=UWF9f5RP25eXMr5i3D3r9uFTtDgL6ex0fFxeN8ijQDXel3I&vprv=1&svpuc=1&mime=audio%2Fmp4&ns=rIfTqPX3iGRjpdwNKy3gEa0Q&gir=yes&clen=4237621&dur=261.702&lmt=1698926110625727&mt=1710784170&fvip=3&keepalive=yes&c=WEB&sefc=1&txp=2318224&n=KqUtaSk_4PCttA&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=ALClDIEwRQIhAOhWICtCkGN91XvTyc8fIgC0uilYcXYk6LVk7ZrzK_T-AiBFwoaD6s-xrCTD02bqOHCJbIuOItLaNmHB2y7USsJp7A%3D%3D&sig=AJfQdSswRQIgHiBImaJbgAkttGLlaiK_BFM-S-lf9x_fCDtoJ9hJLT4CIQDLD4y7e8enCTvoJcdWuJt90iE4UCYRC8w5ADWnUd8OEg%3D%3D');
+  let header: any = {}
 
-  const blob = new Blob(response, { type: 'audio/mp3' })
+  Object.keys(responseInfo.headers).forEach(i => {
+    header[i] = responseInfo.headers[i]
+  })
 
-  return new Response(blob, {
-    status: 200,
-    statusText: 'OK',
-})
-
-  // const isSafari = req.request.headers.get('isSafari') == '1' ? true : false
-  // const video_url = new URL(req.url).searchParams.get('id') ?? ''
-  // const videoId = video_url.length > 20 ? video_url : atob(video_url)
-  // const isSameServer = await isSameServerIp(req.cookies.get('i') ?? ``)
-
-  // if (isFromZeneOrigin(req) === false) json({})
-
-  // try {
-  //   if (videoId.length > 20 && videoId.split('-').length > 3) {
-  //     const radio = new RadioBrowserImpl()
-  //     const response = await radio.radioPlayURL(videoId)
-  //     const url = response[0].url_resolved == null ? response[0].url : response[0].url_resolved
-  //     return json(new DURLResponse(url ?? '', 3))
-  //   }
-
-  //   const ytDownloader = new YTDownloaderImpl()
-  //   const path = await ytDownloader.videoYTDownloader(videoId, isSameServer, isSafari)
-
-  //   const data  = got.stream(path!)
-
-  //   return new Response(path, {
-  //     headers: {
-  //       'Content-Type': 'audio/mpeg'
-  //     }
-  //   })
-
-
-  //   console.log('vdgbhd')
-  //   return new Response(
-  //     path, {
-  //     status: 200,
-  //     headers: {
-  //       'Content-Type': 'application/octet-stream',
-  //       'Content-Disposition':
-  //         `attachment; filename*=UTF-8''${encodeURIComponent('mp3-player')}`,
-  //     }
-  //   }
-  //   );
-
-
-    // const url = await ytDownloader.videoURL(videoId, isSameServer, isSafari)
-
-    // if (url === '') return json({})
-    // let urlPoint = ''
-    // let type = 0
-
-    // if (url.includes('srvcdn7.2convert.me/dl?')) {
-    //   urlPoint = url.textAfterKeyword('srvcdn7.2convert.me/dl?hash=') ?? ''
-    //   type = 0
-    // } else if (url.includes('wsnd.io/')) {
-    //   urlPoint = url.textAfterKeyword('wsnd.io/')?.replaceAll('/videoplayback.mp4', '') ?? ''
-    //   type = 1
-    // } else if (url.includes('ca3.converter.app/')) {
-    //   urlPoint = url.textAfterKeyword('jobid=')
-    //   type = 2
-    // } else {
-    //   urlPoint = url
-    //   type = 3
-    // }
-
-    // return json(new DURLResponse(urlPoint, type))
-  // } catch (error) {
-  //   return json({})
-  // }
+  return new Response(blob, { status: 200, headers: header })
 }
