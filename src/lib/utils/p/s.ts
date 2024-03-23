6e6a591b746877ce1b2330865ed8a4840f70d719
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-03-23 12:34:21 +0530

    updated timeout of axios

--- src/lib/utils/p/s.ts
+++ src/lib/utils/p/s.ts
@@ -106,7 +106,8 @@ export class APManager implements AudioPlayer {
 
     this.audioElement!.autoplay = true
     this.sourceElementMPEG!.src = url.trim()
-    this.audioElement!.muted = true
+    // this.audioElement!.muted = true
+    this.updatemetadata(this.music!)
     this.audioElement!.load()
     this.playbackSpeed()
   }
