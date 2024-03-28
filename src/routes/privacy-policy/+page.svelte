97690ab9f28bd3ea4727856096c723084eca3555
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-03-28 05:31:57 +0530

    fixed the wrong spelling in titles

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <svelte:head>
-  <title>Privacy Policy | Zene: A Free Muic App</title>
+  <title>Privacy Policy | Zene: A Music App</title>
   <meta name="description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
   <meta name="og:description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
-  <meta property="og:title" content="Privacy Policy | Zene: A Free Muic App" />
+  <meta property="og:title" content="Privacy Policy | Zene: A Music App" />
   <meta property="og:image" content="https://zenemusic.co/logo820.png" />
   <meta property="og:image:alt" content="Zene Logo" />
   <link rel="canonical" href="https://zenemusic.co/privacy-policy" />
