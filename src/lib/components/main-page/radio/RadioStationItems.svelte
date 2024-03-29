6616c469f564c244e670ae0dd8e3a38c6e953bc2
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-03-29 14:22:40 +0530

    working on users party

--- src/lib/components/main-page/radio/RadioStationItems.svelte
+++ src/lib/components/main-page/radio/RadioStationItems.svelte
@@ -9,7 +9,7 @@
 <button on:click|stopPropagation={() => playSongZene(musicData, null)}>
   <div class="flex-none py-6 px-1 first:pl-2 last:pr-3">
     <div class="flex flex-col items-center justify-center gap-2 cursor-pointer">
-      <div class={`relative w-[16rem] h-[14rem] bg-maincolor rounded-lg p-3`}>
+      <div class='relative w-[16rem] h-[14rem] bg-maincolor rounded-lg p-3'>
         <button on:click|stopPropagation={() => openSongDialog(musicData)}><img src={MenuIcon} class="absolute top-3 left-3 size-6" alt="menu" /></button>
         <div class="flex flex-1 h-[10rem] justify-center items-center">
           <div class="m-auto flex-col justify-center items-center">
