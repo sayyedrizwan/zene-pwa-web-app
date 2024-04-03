6c8ab56599fb13646c42543e9f466f3c3c157a90
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-04-03 18:15:46 +0530

    rm beta

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -6,26 +6,67 @@ export async function GET({ setHeaders }) {
     <sitemap>
       <loc>https://www.zenemusic.co/sitemap/sitemap.xml</loc>
       <changefreq>weekly</changefreq>
+      <priority>1.00</priority>
     </sitemap>
     <sitemap>
       <loc>https://www.zenemusic.co/sitemap/sitemap-h.xml</loc>
       <changefreq>weekly</changefreq>
+      <priority>0.80</priority>
     </sitemap>
     <sitemap>
       <loc>https://www.zenemusic.co/sitemap/sitemap-b.xml</loc>
       <changefreq>weekly</changefreq>
+      <priority>0.80</priority>
     </sitemap>
     <sitemap>
       <loc>https://www.zenemusic.co/sitemap/sitemap-k.xml</loc>
       <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
     </sitemap>
     <sitemap>
       <loc>https://www.zenemusic.co/sitemap/sitemap-br.xml</loc>
       <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
     </sitemap>
     <sitemap>
       <loc>https://www.zenemusic.co/sitemap/sitemap-hgkg.xml</loc>
       <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
+    </sitemap>
+    <sitemap>
+      <loc>https://www.zenemusic.co/sitemap/sitemap-ch.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
+    </sitemap>
+    <sitemap>
+      <loc>https://www.zenemusic.co/sitemap/sitemap-fn.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
+    </sitemap>
+    <sitemap>
+      <loc>https://www.zenemusic.co/sitemap/sitemap-gn.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
+    </sitemap>
+    <sitemap>
+      <loc>https://www.zenemusic.co/sitemap/sitemap-it.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
+    </sitemap>
+    <sitemap>
+      <loc>https://www.zenemusic.co/sitemap/sitemap-jp.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
+    </sitemap>
+    <sitemap>
+      <loc>https://www.zenemusic.co/sitemap/sitemap-pk.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
+    </sitemap>
+    <sitemap>
+      <loc>https://www.zenemusic.co/sitemap/sitemap-ru.xml</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.60</priority>
     </sitemap>
   </sitemapindex>`
 
