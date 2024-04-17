f926e92c459ab38c1458bd578b9734857460d7c5
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-04-17 17:32:20 +0530

    updated page title

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -44,10 +44,10 @@
 </script>
 
 <svelte:head>
-  <title>Settings | Zene a Music App</title>
+  <title>Settings | Zene : A Music App</title>
   <meta name="description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
   <meta name="og:description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
-  <meta property="og:title" content="Settings | Zene a Music App" />
+  <meta property="og:title" content="Settings | Zene : A Music App" />
   <meta property="og:image" content="https://zenemusic.co/logo820.png" />
   <meta property="og:image:alt" content="Zene Logo" />
   <link rel="canonical" href="https://zenemusic.co/settings" />
