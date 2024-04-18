79961eaf4194a2c58a8aff7e00efc6b8b2e3be7d
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-04-19 00:31:11 +0530

    updated npm and moodlists.

--- src/lib/utils/Utils.ts
+++ src/lib/utils/Utils.ts
@@ -44,6 +44,7 @@ function lastSyncTimeCheck() {
   if (timeDifferenceInMinutes > 180) window.location.reload()
 }
 
+
 export function isRunOnPWA(): Boolean {
   const isInStandaloneMode = () => window.matchMedia('(display-mode: standalone)').matches || (window.navigator as any).standalone || document.referrer.includes('android-app://')
   return isInStandaloneMode()
