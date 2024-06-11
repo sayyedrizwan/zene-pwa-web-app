0fdf29b2fae602ac5b6af5396b7d87d381cbcfe2
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-06-11 20:47:31 +0530

    added processrequest

--- src/routes/api/processrequest/+server.ts
+++ src/routes/api/processrequest/+server.ts
@@ -11,6 +11,7 @@ export async function POST(events: RequestEvent) {
   console.log(body)
   try {
     const r = type == "post" ? await fetch(url ?? "", { method: 'POST', headers: body.headers.toString(), body: body.body.toString() }) : await fetch(url ?? "")
+    console.log(await r.json())
     return restype == "json" ? (await r.json()) : (await r.text())
   } catch (error) {
     return json(apiError)
