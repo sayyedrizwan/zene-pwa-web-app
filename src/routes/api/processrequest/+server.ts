907573bf51a631132fa4ab507cd8b72254061c4a
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-06-11 20:26:40 +0530

    added processrequest

---
+++ src/routes/api/processrequest/+server.ts
@@ -0,0 +1,16 @@
+import { json, type RequestEvent } from '@sveltejs/kit'
+import { apiError} from '../utils/utils'
+
+export async function POST(events: RequestEvent) {
+  const url = events.request.headers.get("url")
+  const type = events.request.headers.get("type")
+  const restype = events.request.headers.get("type")
+
+  const body = await events.request.json()
+  try {
+    const r = type == "post" ? await fetch(url ?? "", { method: 'POST', headers: body.headers, body: body.body }) : await fetch(url ?? "")
+    return restype == "json" ? (await r.json()) : (await r.text())
+  } catch (error) {
+    return json(apiError)
+  }
+}
