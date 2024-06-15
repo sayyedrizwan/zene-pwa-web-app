78236a1959d727a8b5d9f06adff7af37eef5cc3e
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-06-15 15:41:26 +0530

    updated song url player

---
+++ src/routes/api/sduapp/+server.ts
@@ -0,0 +1,22 @@
+import { type RequestEvent } from '@sveltejs/kit'
+import { downloadBlobInChunks, YTDownloaderImpl } from '../api_impl/yt_downloader/YtDownloaderImpl'
+import axios from 'axios'
+
+export const GET = async (req: RequestEvent) => {
+  const videoId = new URL(req.url).searchParams.get('id') ?? ''
+
+  const ytDownloader = new YTDownloaderImpl()
+  const url = await ytDownloader.videoURL(videoId, false)
+  const responseInfo = await axios.head(url!)
+
+  const response = await downloadBlobInChunks(url!, 2000000, responseInfo.headers['content-length'])
+  const blob = new Blob(response, { type: 'audio/mp4' })
+
+  let header: any = {}
+
+  Object.keys(responseInfo.headers).forEach((i) => {
+    header[i] = responseInfo.headers[i]
+  })
+
+  return new Response(blob, { status: 200, headers: header })
+}
