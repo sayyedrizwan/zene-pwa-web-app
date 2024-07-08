470dd822c23cbbe67890fc9373f1495196778cfb
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-07-09 02:18:22 +0530

    completed zusers.

--- src/routes/-api-/utils/Utils.ts
+++ src/routes/-api-/utils/Utils.ts
@@ -1,10 +1,14 @@
 import mysql from 'mysql2/promise';
 
-export const mysqlpool = mysql.createPool({
-    host: 'j3v.h.filess.io',
-    user: 'zenemusic_tapechest',
-    database: 'zenemusic_tapechest',
-    password: 'a33f2d3fbf728e74cc42d3c82e64cacebf358422',
+const isDev = true
+
+//aiven
+export const mysqlpool : any = mysql.createPool({
+    host: isDev ? 'mysql-2bab654c-sayyedrizwanahme-db86.h.aivencloud.com' : 'j3v.h.filess.io',
+    user: isDev ? 'avnadmin' : 'zenemusic_tapechest',
+    database: isDev ? 'users' : 'zenemusic_tapechest',
+    password: isDev ? 'AVNS_52AdOsyCcYA1DBd0DJL' : 'a33f2d3fbf728e74cc42d3c82e64cacebf358422',
+    port: isDev ? 18874 : 3307,
     waitForConnections: true,
     connectionLimit: 3,
     maxIdle: 3,
--- src/routes/-api-/zuser/+server.ts
+++ src/routes/-api-/zuser/+server.ts
@@ -1,7 +1,27 @@
 import { json } from '@sveltejs/kit'
+import { mysqlpool } from '../utils/Utils.js';
 
 export async function POST({ request }) {
 	const body = await request.json()
-	console.log(body)
-	return json({"sss": ""})
+	
+	console.log(Date.now())
+	try {
+		const [results] = await mysqlpool.query('SELECT * FROM `users` WHERE email = ?', [body.email])
+		
+		if(results.length > 0) {
+			await mysqlpool.query('UPDATE `users` SET name = ?, total_playtime = ?, latest_songs = ?, profile_photo = ?, ' + 
+				'last_seen = ?, fcm_token = ?, ip = ?, device_info = ?, country = ?, is_review_done = ?, ' + 
+				'pinned_artists = ? WHERE email = ?', [body.name, body.playtime, JSON.stringify(body.songs), body.photo, 
+					Date.now(), body.fcm, body.ip, body.device, body.country, body.review, JSON.stringify(body.artists), body.email])
+		} else {
+			await mysqlpool.query("INSERT INTO `users` (`email`, `name`, `total_playtime`, `latest_songs`," + 
+				"`profile_photo`, `sign_up_date`, `last_seen`, `fcm_token`, `ip`, `device_info`, `country`, `is_review_done`," + 
+				"`pinned_artists`, `subscription_status`, `subscription_expiry_date`) " +
+				"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [body.email, body.name, 0, "[]",
+					body.photo, Date.now(), Date.now(), body.fcm, body.ip, body.device, body.country, false, '[]', 'FREEMIUM', Date.now()])
+		}
+		return json({"status": "success"})
+	  } catch (err) {
+		return json({"status": err})
+	  }
 }
