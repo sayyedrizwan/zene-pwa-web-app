25a34c5ea9de4473420ead8ce5209ebc0945976e
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-07-21 10:32:47 +0530

    completed the mongodb services..

--- src/routes/-api-/ApiService/dbmongo/MongoDBLocalService.ts
+++ src/routes/-api-/ApiService/dbmongo/MongoDBLocalService.ts
@@ -12,24 +12,34 @@ export class MongoDBLocalService {
 
     collectionSongHistory = mongoDBClient.db(this.mainDBName).collection(this.userSongHistoryDB)
 
-    async updateOrInsertMusicHistory(music: MusicData, email: String, deviceInfo: String, deviceType: String, playTime: number) {
+    async updateOrInsertSongHistory(music: MusicData, email: String, deviceInfo: String, playTime: number) {
         try {
-            
-            const data = new DBMusicHistory(email, music.name, music.artists, music.id, music.thumbnail, deviceInfo, deviceType, Date.now(), playTime)
+
+            const data = new DBMusicHistory(email, music.name, music.artists, music.id, music.thumbnail, deviceInfo, Date.now(), playTime)
             await this.collectionSongHistory.insertOne(data)
         } catch (error) {
             console.log(error)
         }
     }
-    
-    
-    async isSongAlreadyPresentDelete(songID: String, email: String): Promise<DBMusicHistory | undefined>{
+
+
+    async isSongAlreadyPresentDelete(songID: String, email: String): Promise<DBMusicHistory | undefined> {
         try {
             const data = await this.collectionSongHistory.findOne({ id: songID, email: email })
             await this.collectionSongHistory.deleteMany({ id: songID, email: email })
             return data as any
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    async readSongHistory(email: String, page: number): Promise<DBMusicHistory[] | undefined> {
+        try {
+            const skip = page * 50
+            const data = await this.collectionSongHistory.find({ email: email }).sort({timestamp: -1}).skip(skip).limit(50).toArray()
+            return data as any
+        } catch (error) {
+            return []
         }
     }
 }
--- src/routes/-api-/zuser/songhistory/+server.ts
+++ src/routes/-api-/zuser/songhistory/+server.ts
@@ -1,26 +1,38 @@
-import { json } from '@sveltejs/kit'
-import { mysqlpool, verifyHeader } from '../../utils/Utils.js'
+import { verifyHeader } from '../../utils/Utils.js'
 import { MongoDBLocalService } from '../../ApiService/dbmongo/MongoDBLocalService.js'
 import { YoutubeMusicService } from '../../ApiService/youtubemusic/YoutubeMusicService.js'
+import { json } from '@sveltejs/kit'
+
+export async function GET({ request, url }) {
+	if (!verifyHeader(request)) return json([])
+
+	const email = url.searchParams.get('email') ?? ""
+	if (!email.includes("@") && email.length < 3) return json({})
+
+	const page = url.searchParams.get('page') ?? 0
+
+	const list = await MongoDBLocalService.instance.readSongHistory(email, page as number)
+
+	return json(list)
+}
 
 export async function POST({ request }) {
-	if (!verifyHeader(request)) return new Response("error")
+	if (!verifyHeader(request)) return json({ status: false })
 
 	const body = await request.json()
 	const email = body.email as string
 	const songID = body.songID as string
 	const device = body.device as string
-	const deviceType = body.deviceType as string
 
-	if (!email.includes("@") && email.length < 3) return new Response("error")
+	if (!email.includes("@") && email.length < 3) return json({ status: false })
 
 	const data = await MongoDBLocalService.instance.isSongAlreadyPresentDelete(songID, email)
 	const songInfo = await YoutubeMusicService.instance.songInfo(songID)
 
-	if(songInfo != undefined) {
-		if (data == null) await MongoDBLocalService.instance.updateOrInsertMusicHistory(songInfo, email, device, deviceType, 1)
-		else MongoDBLocalService.instance.updateOrInsertMusicHistory(songInfo, email, device, deviceType, (data.timesItsPlayed as number) + 1)
+	if (songInfo != undefined) {
+		if (data == null) await MongoDBLocalService.instance.updateOrInsertSongHistory(songInfo, email, device, 1)
+		else MongoDBLocalService.instance.updateOrInsertSongHistory(songInfo, email, device, (data.timesItsPlayed as number) + 1)
 	}
 
-	return new Response("done")
+	return json({ status: true })
 }
