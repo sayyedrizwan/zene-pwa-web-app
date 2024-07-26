872e8b4b78a786397607201ec32a8b65567ec73f
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-07-26 13:05:01 +0530

    updated info.

--- src/routes/-api-/artistsdata/info/+server.ts
+++ src/routes/-api-/artistsdata/info/+server.ts
@@ -5,6 +5,7 @@ import { YoutubeMusicService } from '../../ApiService/youtubemusic/YoutubeMusicS
 import { ArtistsData, ArtistsDataInfo } from '../../ApiService/model/ArtistsData.js'
 import type { MusicData } from '../../ApiService/model/MusicData.js'
 import { SoundAPIService } from '../../ApiService/soundcloud/SoundAPIService.js'
+import type { SocialProfileUserData } from '../../ApiService/soundcloud/model/SocialProfileUserData.js'
 
 export async function POST({ request }) {
     if (!verifyHeader(request)) return json([])
@@ -13,35 +14,45 @@ export async function POST({ request }) {
     const name = body.name
 
     const lastFMURL = await LastFMService.instance.searchArtists(name)
-    const ytMusicURL = await YoutubeMusicService.instance.searchArtistsSpecific(name)
-
+    
     let imgs: String[] = []
     let topSongs: MusicData[] = []
-
-    const infos = await SoundAPIService.instance.socialInfo(name)
-
-    await Promise.all([1, 2].map(async page => {
-        const lastFMImages = await LastFMService.instance.searchImages(lastFMURL, page)
-        lastFMImages.forEach(e => {
-            if (!imgs.some((item) => item === e)) imgs.push(e)
-        })
-    }))
+    let desc: String | null = ""
+    let mainName: String | null = ""
+    let infos: [SocialProfileUserData, number] = [[], 0]
 
 
-    const lastFMTopSongs = await LastFMService.instance.topSongs(lastFMURL)
 
-    await Promise.all(lastFMTopSongs.map(async songs => {
-        const song = await YoutubeMusicService.instance.searchSongs(`${ytMusicURL?.name} - ${songs[0]}`)
-        if (song[0] != undefined) {
-            const s = song[0]
-            s.extra = songs[1]
-            topSongs.push(s)
+    await Promise.all([1, 2, 3, 4, 5].map(async page => {
+        if (page == 1 || page == 2) {
+            const lastFMImages = await LastFMService.instance.searchImages(lastFMURL, page)
+            lastFMImages.forEach(e => {
+                if (!imgs.some((item) => item === e)) imgs.push(e)
+            })
+        } else if (page == 3) {
+            const ytMusicURL = await YoutubeMusicService.instance.searchArtistsSpecific(name)
+            const lastFMTopSongs = await LastFMService.instance.topSongs(lastFMURL)
+
+            mainName = ytMusicURL?.name ?? null
+
+            await Promise.all(lastFMTopSongs.map(async songs => {
+                const song = await YoutubeMusicService.instance.searchSongs(`${ytMusicURL?.name} - ${songs[0]}`)
+                if (song[0] != undefined) {
+                    const s = song[0]
+                    s.extra = songs[1]
+                    topSongs.push(s)
+                }
+            }))
+        } else if (page == 4) {
+            desc = await LastFMService.instance.wiki(lastFMURL) ?? null
+        } else if (page == 5) {
+            infos = await SoundAPIService.instance.socialInfo(name)
         }
     }))
 
-    const desc = await LastFMService.instance.wiki(lastFMURL) ?? null
 
-    if (ytMusicURL?.name == undefined) return json({})
-    return json(new ArtistsDataInfo(ytMusicURL?.name, imgs, topSongs, desc, infos[1], infos[0]))
+    
+    if (mainName == undefined) return json({})
+    return json(new ArtistsDataInfo(mainName, imgs, topSongs, desc, infos[1], infos[0]))
 }
 
