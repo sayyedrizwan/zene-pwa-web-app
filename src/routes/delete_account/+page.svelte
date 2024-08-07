6b4f29c928e2ff6c447b5b79a82454127f19970e
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-08-07 10:18:08 +0530

    updated privacy policy and account deletion

---
+++ src/routes/delete_account/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Footer from "$lib/components/item/Footer.svelte";
+  import { support_mail_server } from "$lib/utils/Utils";
+
+</script>
+
+<svelte:head>
+  <title>Data retention and account termination | Zene : A Music App</title>
+  <meta name="description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
+  <meta name="og:description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
+  <meta property="og:title" content="Data retention and account termination | Zene : A Music App" />
+  <meta property="og:image" content="https://zenemusic.co/logo820.png" />
+  <meta property="og:image:alt" content="Zene Logo" />
+  <link rel="canonical" href="https://zenemusic.co/delete_account" />
+  <meta name="keywords" content="zene, zene free music, zene a free music, free music, zene, zene songs, zene music, music streaming, music streaming app, free music streaming, music app for android, music app for iphone, music player app, free music download, music discovery app, offline listening, curated playlists, high-fidelity audio, free music streaming with no ads, workout music app, music for studying app, audio streaming, mobile music app, radio, radio streaming" />
+</svelte:head>
+
+
+<div class="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-24 text-white">
+  <h1 class="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">Data retention and account termination</h1>
+  <p class="mt-4 text-base leading-7 text-slate-600">Last updated on Aug 12, 2024</p>
+</div>
+
+
+<div class="mx-auto max-w-7xl prose-sm prose prose-slate prose-a:font-semibold prose-a:text-sky-500 hover:prose-a:text-sky-600 text-white font-normal">
+  <p>
+    You can close your account by visiting and filling out <a href="https://forms.gle/xNjhoquxqQ4S2JfC9" class="text-lightblue">this form.</a>
+    <br /><br />
+      We will remove your public posts from view and/or dissociate them from your account profile, but we may retain information about you for the purposes authorized under this Privacy Policy unless prohibited by law. Thereafter, we will either delete your personal information or de-identify it so that it is anonymous and not attributed to your identity. For example, we may retain information to prevent, investigate, or identify possible wrongdoing in connection with the Service or to comply with legal obligations.
+    <br /><br />
+    <br />
+    If you have any queries relating to the processing/ usage of information provided by you or Zene's Privacy Policy, you may email the Data Protection Officer (DPO) at knocknock@zenemusic.co or write to us at the following address.
+    <br /><br />
+    </p>
+</div>
+
+<Footer />
