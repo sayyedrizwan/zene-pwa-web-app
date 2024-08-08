f287bd77bb0417bf81218758ec4d6636062d025b
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-08-08 09:57:02 +0530

    updated database

--- src/routes/-api-/utils/Utils.ts
+++ src/routes/-api-/utils/Utils.ts
@@ -3,9 +3,11 @@ import os from 'os'
 import { MongoClient } from 'mongodb'
 import type { MusicData } from '../ApiService/model/MusicData';
 
-export const isDevDB = !(process.platform.toLowerCase() == "darwin")
+export const isDevDB = (process.platform.toLowerCase() == "darwin")
 const auth = "qtASBDg887XCIJBVB112Pl.AYuhn2e"
 export const zenePlaylistsParam = "zene_p_"
+
+
 
 
 //aiven
