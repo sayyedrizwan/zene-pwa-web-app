2b7d121f88357aa0b60cb7b6c3b1295c7bc2120e
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-08-13 10:37:45 +0530

    updated

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -5,19 +5,19 @@
 </script>
 
 <svelte:head>
-  <title>Privacy Policy | Zene : A Music App</title>
+  <title>Legal | Zene : A Music App</title>
   <meta name="description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
   <meta name="og:description" content="Discover the top music streaming platforms to find your favorite artists, albums, and playlists." />
-  <meta property="og:title" content="Privacy Policy | Zene : A Music App" />
+  <meta property="og:title" content="Legal | Zene : A Music App" />
   <meta property="og:image" content="https://zenemusic.co/logo820.png" />
   <meta property="og:image:alt" content="Zene Logo" />
-  <link rel="canonical" href="https://zenemusic.co/privacy-policy" />
+  <link rel="canonical" href="https://zenemusic.co/legal" />
   <meta name="keywords" content="zene, zene free music, zene a free music, free music, zene, zene songs, zene music, music streaming, music streaming app, free music streaming, music app for android, music app for iphone, music player app, free music download, music discovery app, offline listening, curated playlists, high-fidelity audio, free music streaming with no ads, workout music app, music for studying app, audio streaming, mobile music app, radio, radio streaming" />
 </svelte:head>
 
 
 <div class="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-24 text-white">
-  <h1 class="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">Privacy Policy</h1>
+  <h1 class="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">Account Delet</h1>
   <p class="mt-4 text-base leading-7 text-slate-600">Last updated on Aug 12, 2024</p>
 </div>
 
