de1aed06f63948b5dbad9ad0431c0a7ae03eed38
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-08-18 17:56:20 +0530

    updated the logs of ads api.

---
+++ src/routes/-api-/zuser/add_ads_logs/+server.ts
@@ -0,0 +1,22 @@
+import { json } from "@sveltejs/kit";
+import { heartbeatAPI, mysqlpool, verifyHeader } from "../../utils/Utils.js";
+import { MySqlLocalService } from "../../ApiService/dbmysql/MySqlLocalService.js";
+
+export async function POST({ request }) {
+  // heartbeatAPI("zuser-add-user");
+  if (!verifyHeader(request)) return json({ status: "error" });
+
+  const body = await request.json();
+  const email = body.email as string;
+
+  if (!email.includes("@") && email.length < 3) return json({ status: "error" });
+
+  try {
+    await mysqlpool.query("INSERT INTO `rewards_ads_logs` (`id`, `watched_timestamp`, `email`) VALUES (?, ?, ?)", [null, Date.now(), email]);
+
+    return json({ status: "success" });
+  } catch (err) {
+    console.log(err);
+    return json({ status: "error" });
+  }
+}
