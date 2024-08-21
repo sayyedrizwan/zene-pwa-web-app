a4d9c68928854c155fb8d3cc31122423fe86d8fa
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-08-21 13:03:44 +0530

    updated part names

--- src/routes/-api-/zuser/users/+server.ts
+++ src/routes/-api-/zuser/users/+server.ts
@@ -9,7 +9,6 @@ export async function GET({ url, request }) {
 	const email = url.searchParams.get('user') ?? ""
 	if (!email.includes("@") && email.length < 3) return json({})
 
-	console.log(email)
 	try {
 		const userInfo = await MySqlLocalService.instance.searchUser(email)
 		return json(userInfo)
