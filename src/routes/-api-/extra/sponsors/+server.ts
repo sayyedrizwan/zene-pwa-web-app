c6536088d1965a7da310ff28c10aaaf536516e09
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-09-18 00:29:04 +0530

    working on sponsers ads,

---
+++ src/routes/-api-/extra/sponsors/+server.ts
@@ -0,0 +1,66 @@
+import { json } from "@sveltejs/kit";
+
+export async function GET() {
+  const imgsData = [
+    {
+      link: "https://www.zenemusic.co/",
+      img: "https://i.pinimg.com/564x/94/fd/85/94fd85c648b6acb11c2fb52bd99ae8d0.jpg",
+    },
+    {
+      link: "https://www.zenemusic.co/",
+      img: "https://i.pinimg.com/564x/94/fd/85/94fd85c648b6acb11c2fb52bd99ae8d0.jpg",
+    },
+    {
+      link: "https://www.zenemusic.co/",
+      img: "https://i.pinimg.com/originals/06/9d/61/069d617dca720be2d65014963515d28e.gif",
+    },
+    {
+      link: "https://www.zenemusic.co/",
+      img: "https://i.pinimg.com/564x/94/fd/85/94fd85c648b6acb11c2fb52bd99ae8d0.jpg",
+    },
+    {
+      link: "https://www.zenemusic.co/",
+      img: "https://i.pinimg.com/564x/94/fd/85/94fd85c648b6acb11c2fb52bd99ae8d0.jpg",
+    },
+    {
+      link: "https://www.zenemusic.co/",
+      img: "https://i.pinimg.com/originals/06/9d/61/069d617dca720be2d65014963515d28e.gif",
+    },
+    {
+      link: "https://www.zenemusic.co/",
+      img: "https://i.pinimg.com/564x/94/fd/85/94fd85c648b6acb11c2fb52bd99ae8d0.jpg",
+    },
+  ];
+
+  const jsonAdsInfo = {
+    top: {
+      title: "Helloo soo what's up",
+      imgs: imgsData,
+    },
+
+    bottom: {
+      title: "Helloo soo what's up",
+      imgs: imgsData,
+    },
+  };
+
+  const jsonData = {
+    ios: {
+      showAds: true,
+      showSponserAds: true,
+      ads: jsonAdsInfo,
+    },
+    android: {
+      showAds: true,
+      showSponserAds: true,
+      ads: jsonAdsInfo,
+    },
+    web: {
+      showAds: true,
+      showSponserAds: true,
+      ads: jsonAdsInfo,
+    },
+  };
+
+  return json(jsonData);
+}
