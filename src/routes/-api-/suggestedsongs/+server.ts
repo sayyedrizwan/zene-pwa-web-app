44b2c6054ee3bdb8d80adedbf36aec8a2e364df4
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-09-18 22:11:33 +0530

    added suggested songs..

--- src/routes/-api-/suggestedsongs/+server.ts
+++ src/routes/-api-/suggestedsongs/+server.ts
@@ -1,36 +1,39 @@
-import { json } from '@sveltejs/kit'
-import type { MusicData } from '../ApiService/model/MusicData.js'
-import { heartbeatAPI, shuffle, verifyHeader } from '../utils/Utils.js'
-import { YoutubeMusicService } from '../ApiService/youtubemusic/YoutubeMusicService.js'
-import { MongoDBLocalService } from '../ApiService/dbmongo/MongoDBLocalService.js'
+import { json } from "@sveltejs/kit";
+import type { MusicData } from "../ApiService/model/MusicData.js";
+import { heartbeatAPI, shuffle, verifyHeader } from "../utils/Utils.js";
+import { YoutubeMusicService } from "../ApiService/youtubemusic/YoutubeMusicService.js";
+import { MongoDBLocalService } from "../ApiService/dbmongo/MongoDBLocalService.js";
 
 export async function POST({ request }) {
-    heartbeatAPI("suggested-songs");
-    if (!verifyHeader(request)) return json([])
+  heartbeatAPI("suggested-songs");
+  if (!verifyHeader(request)) return json([]);
 
-    const body = await request.json()
+  const body = await request.json();
 
-    if (!body.email.includes("@") && body.email.length < 3) return json([])
-    let songsID = await MongoDBLocalService.instance.topFifteenSongsOfUsers(body.email)
+  if (!body.email.includes("@") && body.email.length < 3) return json([]);
+  let songsID = await MongoDBLocalService.instance.topFifteenSongsOfUsers(body.email);
 
+  let list: MusicData[] = [];
 
-    let list: MusicData[] = []
+  await Promise.all(
+    songsID.map(async (id: String) => {
+      try {
+        if (id != null || id != undefined) {
+          const sSongs = await YoutubeMusicService.instance.similarSongs(id.toString());
+          const nSongs = await YoutubeMusicService.instance.upNextSongs(id.toString());
 
-    await Promise.all(songsID.map(async (id: String) => {
-        try {
-            const sSongs = await YoutubeMusicService.instance.similarSongs(id.toString())
-            const nSongs = await YoutubeMusicService.instance.upNextSongs(id.toString())
-
-            sSongs?.forEach((p, i) => {
-                if (i > 3 && !list.some((item) => item.id === p.id)) list.push(p)
-            })
-            nSongs?.forEach((p, i) => {
-                if (i > 3 && !list.some((item) => item.id === p.id)) list.push(p)
-            })
-        } catch (error) {
-            console.log(error)
+          sSongs?.forEach((p, i) => {
+            if (i > 3 && !list.some((item) => item.id === p.id)) list.push(p);
+          });
+          nSongs?.forEach((p, i) => {
+            if (i > 3 && !list.some((item) => item.id === p.id)) list.push(p);
+          });
         }
-    }))
+      } catch (error) {
+        console.log(error);
+      }
+    })
+  );
 
-    return json(shuffle(list))
+  return json(shuffle(list));
 }
