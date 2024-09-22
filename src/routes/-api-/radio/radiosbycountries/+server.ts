9d7c750c1ed5095bec39f4bab0d445c9a24e76cf
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-09-22 19:25:31 +0530

    completed then radio api..

---
+++ src/routes/-api-/radio/radiosbycountries/+server.ts
@@ -0,0 +1,15 @@
+import { json } from "@sveltejs/kit";
+import { RadioAPIService } from "../../ApiService/radio/RadioAPIService.js";
+import { heartbeatAPI, verifyHeader } from "../../utils/Utils.js";
+
+export async function POST({ request }) {
+  heartbeatAPI("radio-top");
+  if (!verifyHeader(request)) return json([]);
+
+  const body = await request.json();
+  const countryID = body.countryID;
+  const page = body.page;
+  
+  const radios = await RadioAPIService.instance.radioForCountriesViaTrend(countryID, page);
+  return json(radios);
+}
