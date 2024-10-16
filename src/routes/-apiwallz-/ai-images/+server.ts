72ed02970bcb05f4667d1c75943a85f0decb9c0b
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-10-16 18:40:56 +0530

    updated the ai images.

--- src/routes/-apiwallz-/ai-images/+server.ts
+++ src/routes/-apiwallz-/ai-images/+server.ts
@@ -3,32 +3,34 @@ import { verifyHeader } from "../../-api-/utils/Utils";
 import type { WallpaperData } from "../WallzApiService/MySQLService/model/WallpaperData";
 import { WallzAiService } from "../WallzApiService/WallzAiAdobe/WallzAiService";
 import { getAIWallpaperSaved, saveAIWallaperListWithTimestamp } from "../utils/TempData";
+import { shuffle } from "../utils/Utils";
 
 export async function GET({ request }) {
   if (!verifyHeader(request)) return json([]);
 
-  const l = getAIWallpaperSaved();
-  if (l.length > 0) return json(l);
+  // const l = getAIWallpaperSaved();
+  // if (l.length > 0) return json(l);
 
-  const lists: WallpaperData[] = [];
+  // const lists: WallpaperData[] = [];
 
-  let lastID = "";
+  // let lastID = "";
 
-  for (let i = 0; i < 9; i++) {
-    const l = await WallzAiService.instance.getAINightCafe(lastID, "hottest", i);
-    lastID = l[1].toString();
-    l[0].map((v) => (!lists.some((item) => item.id === v.id) ? lists.push(v) : null));
-  }
+  // for (let i = 0; i < 9; i++) {
+  //   const l = await WallzAiService.instance.getAINightCafe(lastID, "hottest", i);
+  //   lastID = l[1].toString();
+  //   l[0].map((v) => (!lists.some((item) => item.id === v.id) ? lists.push(v) : null));
+  // }
 
-  for (let i = 0; i < 9; i++) {
-    const l = await WallzAiService.instance.getAINightCafe(lastID, "new", i);
-    lastID = l[1].toString();
-    l[0].map((v) => (!lists.some((item) => item.id === v.id) ? lists.push(v) : null));
-  }
+  // for (let i = 0; i < 9; i++) {
+  //   const l = await WallzAiService.instance.getAINightCafe(lastID, "new", i);
+  //   lastID = l[1].toString();
+  //   l[0].map((v) => (!lists.some((item) => item.id === v.id) ? lists.push(v) : null));
+  // }
 
-  const listsTemp = lists.length > 15 ? lists.slice(0, 15) : lists
-  //  shuffle(lists);
-  saveAIWallaperListWithTimestamp(listsTemp);
+  // const listsTemp = shuffle(lists);
+  // saveAIWallaperListWithTimestamp(listsTemp);
+
+
   return json({});
 }
 
