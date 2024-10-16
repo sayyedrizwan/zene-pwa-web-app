c2114cd3711ae58d4c9612ebe118cb6b9e2dc3c5
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-10-16 18:51:16 +0530

    completed the wallpaper ai images

--- src/routes/-apiwallz-/ai-images/+server.ts
+++ src/routes/-apiwallz-/ai-images/+server.ts
@@ -1,6 +1,6 @@
 import { json } from "@sveltejs/kit";
 import { verifyHeader } from "../../-api-/utils/Utils";
-import type { WallpaperData } from "../WallzApiService/MySQLService/model/WallpaperData";
+import { WallpaperData } from "../WallzApiService/MySQLService/model/WallpaperData";
 import { WallzAiService } from "../WallzApiService/WallzAiAdobe/WallzAiService";
 import { getAIWallpaperSaved, saveAIWallaperListWithTimestamp } from "../utils/TempData";
 import { shuffle } from "../utils/Utils";
@@ -30,8 +30,18 @@ export async function GET({ request }) {
   // const listsTemp = shuffle(lists);
   // saveAIWallaperListWithTimestamp(listsTemp);
 
+  const aiImages :WallpaperData[] = [];
 
-  return json({});
+  aiImages.push(new WallpaperData("https://creator.nightcafe.studio/creation/SuM3Q2L76KS3t8mOnlEO", "", "https://images.nightcafe.studio/jobs/SuM3Q2L76KS3t8mOnlEO/SuM3Q2L76KS3t8mOnlEO--1--qlad7.jpg?tr=w-640,c-at_max", ""))
+  aiImages.push(new WallpaperData("https://creator.nightcafe.studio/creation/yVe8yukkN5aNRKr5I21Y", "", "https://images.nightcafe.studio/jobs/yVe8yukkN5aNRKr5I21Y/yVe8yukkN5aNRKr5I21Y--1--q5evm_1.5x-clty-upscale-hvokk.jpg?tr=w-640,c-at_max", ""))
+  aiImages.push(new WallpaperData("https://creator.nightcafe.studio/creation/mS0Fm4JZb5MWN9XlVs12", "", "https://images.nightcafe.studio/jobs/mS0Fm4JZb5MWN9XlVs12/mS0Fm4JZb5MWN9XlVs12--1--k2ryw.jpg?tr=w-640,c-at_max", ""))
+  aiImages.push(new WallpaperData("https://creator.nightcafe.studio/creation/zZjglvuWwG2ILgrqVADp", "", "https://images.nightcafe.studio/jobs/zZjglvuWwG2ILgrqVADp/zZjglvuWwG2ILgrqVADp-DkldQ-adjusted.jpg?tr=w-640,c-at_max", ""))
+  aiImages.push(new WallpaperData("https://creator.nightcafe.studio/creation/kZrUVSjHKHoGuwso2F8z", "", "https://images.nightcafe.studio/jobs/kZrUVSjHKHoGuwso2F8z/kZrUVSjHKHoGuwso2F8z--1--ix0cu_2x-clty-upscale-0rpkc.jpg?tr=w-640,c-at_max", ""))
+  aiImages.push(new WallpaperData("https://creator.nightcafe.studio/creation/Hfg5frzKt1oEvwKvOeOH", "", "https://images.nightcafe.studio/jobs/Hfg5frzKt1oEvwKvOeOH/Hfg5frzKt1oEvwKvOeOH--1--uo4x4_2x-clty-upscale-p9cf2.jpg?tr=w-640,c-at_max", ""))
+  aiImages.push(new WallpaperData("https://creator.nightcafe.studio/creation/sfugO75rsJucabjSZLv5", "", "https://images.nightcafe.studio/jobs/sfugO75rsJucabjSZLv5/sfugO75rsJucabjSZLv5--1--d2xc7.jpg?tr=w-640,c-at_max", ""))
+
+
+  return json(aiImages);
 }
 
 //https://gencraft.com/explore
