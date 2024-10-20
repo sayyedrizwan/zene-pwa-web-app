a2ab87339f1d81de5a4f194af1e47221b1d55927
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-10-20 14:21:31 +0530

    updated a new params

--- src/routes/-apiwallz-/WallzApiService/MySQLService/model/WallpaperData.ts
+++ src/routes/-apiwallz-/WallzApiService/MySQLService/model/WallpaperData.ts
@@ -1,5 +1,6 @@
 export class WallpaperData {
   id?: String;
+  idPath?: String;
   name?: String;
   thumbnail?: String;
   desc?: String;
@@ -9,6 +10,7 @@ export class WallpaperData {
 
   constructor(id?: String, name?: String, thumbnail?: String, desc?: String, page: number = 0, type: WALLPAPERTYPE = WALLPAPERTYPE.IMAGE_WALLPAPER, extra: Boolean = false) {
     this.id = id;
+    this.idPath = id;
     this.name = name;
     this.thumbnail = thumbnail;
     this.desc = desc
