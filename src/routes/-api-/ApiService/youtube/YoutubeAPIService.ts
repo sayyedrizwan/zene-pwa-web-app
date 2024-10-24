43172419aa528008dd53de3cb8ce2cbaf46f4150
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2024-10-24 19:16:29 +0530

    completed the namee

--- src/routes/-api-/ApiService/youtube/YoutubeAPIService.ts
+++ src/routes/-api-/ApiService/youtube/YoutubeAPIService.ts
@@ -96,7 +96,7 @@ export class YoutubeAPIService {
         const finalName = name.includes(artists) ? substringAfter(name, " -").trim() : name
         const thumbnail = `https://i.ytimg.com/vi/${id}/sddefault.jpg`;
 
-        if (id != undefined) lists.push(new MusicData(finalName, artists, id, thumbnail, MUSICTYPE.VIDEO, id))
+        if (id != undefined) lists.push(new MusicData(finalName.length > 2 ? finalName : name, artists, id, thumbnail, MUSICTYPE.VIDEO, id))
       });
 
       return lists
